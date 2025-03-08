2ad4ddd479b63dcf69e3026aceefab8e1477b523
xshoptopup-store <xs.ho.pt.opup.store@gmail.com>
2025-03-09 00:48:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use('/api', router);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/endpoint', (req, res) => {
