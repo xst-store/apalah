4ac76b3d939b512537aa9ab8a4d08e50b11e1f4d
xshoptopup-store <xs.ho.pt.opup.store@gmail.com>
2025-03-09 01:00:04 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path')
+const _ = require('lodash');
+const { dataGame } = require('./utils/data');
+const router = require('./routes');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use('/test', express.static('public'));
+app.use(cors());
+
+app.use('/api', router);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/endpoint', (req, res) => {
+   const newDataGame = dataGame.map((item) => {
+      return {
+         name: item.name,
+         slug: item.slug,
+         endpoint: `/api/game/${item.slug}`,
+         query: `?id=xxxx${item.isZone ? '&zone=xxx' : ''}`,
+         hasZoneId: item.isZone ? true : false,
+         listZoneId: item.dropdown ? `/api/game/get-zone/${item.slug}` : null,
+      };
+   });
+
+   return res.json({
+      name: 'XSTBot Whatsapp',
+      data: _.orderBy(newDataGame, ['name'], ['asc']),
+   });
+});
+
+app.get('/*', (req, res) => {
+   res.status(404).json({ error: 'Error' });
+});
+
+app.listen(port, () => {
+   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+module.exports = app;
